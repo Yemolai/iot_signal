117ab9d1fa320269636ea449a5dd1ad9bbfdd3ab
Gabriel R <yemolai@gmail.com>
2019-08-04 17:04:02 -0300

    alterando nomenclatura e corrigindo verificação de tipo

--- utils/validation.js
+++ utils/validation.js
@@ -1,12 +1,28 @@
 const isPositiveIntNumber = (num) => {
-  return num && Math.floor(Number(num)) === num && Number(num) > 0
+  return isIntNumber(num) && Number(num) > 0
 }
 
-const isNumber = (num) => {
-  return num && Number(num) === num
+const validatePositiveIntNumber = (num, name) => {
+  if (isPositiveIntNumber(num)) {
+    return num
+  }
+  throw new Error(name + ' should be a positive int number')
+}
+
+const isIntNumber = (num) => {
+  return !isNaN(Number(num)) && Number(num) === Number(num)
+}
+
+const validateNumber = (num, name) => {
+  if (isIntNumber(num)) {
+    return num
+  }
+  throw new Error(name + ' should be a number')
 }
 
 module.exports = {
+  validatePositiveIntNumber,
   isPositiveIntNumber,
-  isNumber
+  validateNumber,
+  isIntNumber
 }
